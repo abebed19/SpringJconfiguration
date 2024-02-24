0e0dd22373d6a7ae22d3e26d85e648e0603c4adb
abebed19 <30549320+abebed19@users.noreply.github.com>
2024-02-24 16:32:07 +0300

    Test updated to beans

--- src/com/durgasoft/test/Test.java
+++ src/com/durgasoft/test/Test.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
-//        WelcomeBean welcomeBean=(WelcomeBean) context.getBean("welcomeBean");//we can use method name as bean id
-//        System.out.println( welcomeBean.getWelcomeMessage());
-//        //ALternatively we can use Classname.class to access bean
-//
-//        //Accessing another bean
-//        HelloBean helloBean=(HelloBean) context.getBean(HelloBean.class);
-//        System.out.println(helloBean.sayHello());
+        WelcomeBean welcomeBean=(WelcomeBean) context.getBean("welcomeBean");//we can use method name as bean id
+        System.out.println( welcomeBean.getWelcomeMessage());
+
+
+        //Accessing another bean ALternatively we can use Classname.class to access bean
+        HelloBean helloBean=(HelloBean) context.getBean(HelloBean.class);
+        System.out.println(helloBean.sayHello());
     }
 }
